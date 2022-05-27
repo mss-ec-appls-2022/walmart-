5049d2a20e3fa82db1167a8728ee922174944863
Abinash-Paltasingh <45588602+Abinash-Paltasingh@users.noreply.github.com>
2022-05-27 16:01:08 +0530

    git pull update

--- Utils.java
+++ Utils.java
@@ -5,4 +5,7 @@ utility files
 old line for check the stash
  adding data for git fetch 
  checking it will work or not
+ adding some line for checking git pull work or not 
+ and also checking the difference  between git pull vs git fetch
+ 
  
