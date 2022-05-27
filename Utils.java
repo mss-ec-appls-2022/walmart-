9b37b3418137054848d92bf7cbb6b955710d01b8
devops <abinashpaltasingh@gmail.com>
2022-05-27 11:24:39 +0000

    merge confilict resolved

--- Utils.java
+++ Utils.java
@@ -1,11 +1,11 @@
 
 utility files
  adding new content on file
- new  features devloped
+ new  features devlo
 old line for check the stash
  adding data for git fetch 
  checking it will work or not
  adding some line for checking git pull work or not 
  and also checking the difference  between git pull vs git fetch
- 
- 
+hy this lines for cherry-pic
+
